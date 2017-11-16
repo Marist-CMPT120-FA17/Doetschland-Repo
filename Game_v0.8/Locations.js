15c77b69844458de4592f460f1f0a4b77ce7b880
Cdoetsch <31552105+Cdoetsch@users.noreply.github.com>
2017-11-16 17:42:38 -0500

    V0.8
    I'm super sick so I'm not coming to class but I gotta get this in.

---
+++ Game_v0.8/Locations.js
@@ -0,0 +1,22 @@
+//javascript for Game_v0.8
+
+function location(id, name, description, item,  toString()) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.item = item;
+}
+
+function initialize(){
+	var Castle = new location(1, Castle, You stand in front of);
+	var Farmland = new location();
+	var ForestPath = new location();
+	var River = new location();
+	var Hills = new location();
+	var Temple = new location();
+	var Swamp = new location();
+	var Tiger = new location();
+	var InnerSanctum = new location();
+	var Dragon = new location();
+	
+}
